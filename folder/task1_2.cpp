8951083282a9e24aae1a353c3f2690588bfe378a
Vidzteck <akakiy.kurau@bk.ru>
2025-09-10 14:02:54 +0300

    Update task1_2.cpp

--- folder/task1_2.cpp
+++ folder/task1_2.cpp
@@ -1,10 +1,12 @@
+//реализация пустого списка и пустого стека на C++
 #include <list>
 #include <iostream>
 #include <stack>
 
 int main()
 {
-    std::list<int> list1;  
-    std::stack<std::string> stack;     
+    std::list<int> list1;  //пустой список
+    std::stack<std::string> stack;     //пустой стек
     return 0     
 }
+
